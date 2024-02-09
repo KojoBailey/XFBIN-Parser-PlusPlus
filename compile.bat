982ea0744e1d6d9b7073643b74eaab89a8c4c40b
Kojo Bailey <50509420+KojoBailey@users.noreply.github.com>
2024-02-09 12:18:15 +0300

    Delete compile.bat

--- compile.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-echo Compiling...
-g++ src/main.cpp src/functions.cpp src/templates/messageInfo.cpp src/templates/MainModeParam.cpp src/templates/GuideCharParam.cpp src/templates/Template.cpp -o main.exe -std=c++20 -static -static-libgcc -static-libstdc++
